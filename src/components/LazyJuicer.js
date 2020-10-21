d27a09a92f7457fd26316828c8c412d222c2c7f7
Monkeystation <job@monkeystation.nl>
2020-10-21 16:37:48 +0800

    Update LazyJuicer.js

--- src/components/LazyJuicer.js
+++ src/components/LazyJuicer.js
@@ -9,16 +9,22 @@ const LazyJuicer = (props) => {
 
   useEffect(() => {
     if (inView) {
-      const script = document.createElement('script');
-      script.src = 'https://assets.juicer.io/embed.js';
-      script.async = true;
-      document.body.appendChild(script);
+      const script = document.createElement('script')
+      script.src = 'https://assets.juicer.io/embed.js'
+      script.async = true
+      document.body.appendChild(script)
+
+      const link = document.createElement('link')
+      link.href = 'https://assets.juicer.io/embed.css'
+      link.media = "all"
+      link.rel = "stylesheet"
+      link.type = "text/css"
+      document.body.appendChild(link);
     }
   }, [inView]);
 
   return (
     <div ref={ref}>
-      <link href="https://assets.juicer.io/embed.css" media="all" rel="stylesheet" type="text/css" />
       <ul className="juicer-feed" data-feed-id="membersihkanindonesia" data-per="12">
         <h1 className="referral"><a href="https://www.juicer.io">Powered by Juicer</a></h1>
       </ul>
